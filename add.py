c599f101bc23f9012609a1e849ef2458f29b46ee
mayuresh <mayureshjaywant123@gmail.com>
2024-12-19 15:06:36 +0530

    .gitignore added

--- add.py
+++ add.py
@@ -6,3 +6,17 @@ number_2 = int(input("Enter second number: "))
 
 print(number_1, "+", number_2, "=",
                     add(number_1, number_2))
+
+
+
+# Function to subtract two numbers
+def subtract(num1, num2):
+    return num1 - num2
+
+
+number_1 = int(input("Enter first number: "))
+number_2 = int(input("Enter second number: "))
+
+
+print(number_1, "-", number_2, "=",
+                    subtract(number_1, number_2))
